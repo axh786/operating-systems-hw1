5b28aafd5195968da775b81eab77244f411e50df
axh786 <143470097+axh786@users.noreply.github.com>
2025-02-10 17:19:47 -0600

    Add io capabilities to single thread solution
    use fstream to take in input from txt files and solve the problem with
    the single threaded solution

--- single_thread.cpp
+++ single_thread.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
     // Read first line and make it the size of a dynamic 2d array Eg: 26 7 would be 7 rows and 26 col
     //2nd line tells us where each character can go in a given range both numbers inclusive? U 0 10 means U can go in slots 0-10
     //3rd line and 4th line work togther, 3rd line tells us what range of char so 0 4 8 12... means 0-4 do the first 4 index in 4th line
     //Input is a multiline input string
+    std::ifstream file("input1.txt");
+    
+    file.close();
 }
