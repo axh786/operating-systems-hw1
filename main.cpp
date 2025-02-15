1d7e1d8b97abf1680b71b09d189e178a5b07e231
axh786 <143470097+axh786@users.noreply.github.com>
2025-02-14 22:11:49 -0600

    Add libraries and main to main.cpp
    Use includes to include vector pthread and other essential libraries for
    the programming assignment

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+Ali Husain
+Dr. Rincon
+COSC 3360: Programming Assignment 1
+14 Feb 2025
+*/
+
+#include <pthread.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <vector>
+
+int main() {
+
+}
